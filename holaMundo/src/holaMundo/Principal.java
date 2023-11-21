5a1692cf17bcce3817ffd36657105e4b86494016
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2023-11-21 14:05:17 +0100

    Se mejora el hola mundo

--- holaMundo/src/holaMundo/Principal.java
+++ holaMundo/src/holaMundo/Principal.java
@@ -4,8 +4,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		
+		System.out.println("-".repeat(50));
 		System.out.println("Hola mundo");
+		System.out.println("-".repeat(50));
 
 	}
 
 }
+
