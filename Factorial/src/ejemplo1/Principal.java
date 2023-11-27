d2f6ca90e1809c33c5eb92e79814d57e370fa447
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2023-11-27 13:31:17 +0100

    Ejemplos

---
+++ Factorial/src/ejemplo1/Principal.java
@@ -0,0 +1,10 @@
+package ejemplo1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		MultiFactorial mf = new MultiFactorial(150);
+		mf.imprimeFactorial();
+	}
+		
+}
--- Factoriales/src/module-info.java
+++ Factoriales/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module holaMundo {
+module Factoriales {
 }
